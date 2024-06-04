cddc064af155fe97167c97e3e879f54e72576ec5
Jovana_Matic <jovana.stankovic85@yahoo.com>
2024-06-04 18:47:02 +0200

    Render additional information and include styles

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -7,17 +7,37 @@
   const getChapter = computed(() => courses.chapters.find(chapter => chapter.slug === chapterSlug))
 
   const getLesson = computed(() => getChapter.value.lessons.find(lesson => lesson.slug === lessonSlug))
+  console.log(getLesson.value)
 
 </script>
 
 <template>
-  <div>
-    <h4>Lesson</h4>
-    <p>{{ getLesson.title }}</p>
+  <div class="lesson-number">
+    <p>Lesson {{ getChapter.number }} - {{ getLesson.number }}</p>
+    <h2>{{ getLesson.title }}</h2>
+    <div>
+      <a v-if="getLesson.sourceUrl"
+      :href="getLesson.sourceUrl"
+      >
+      Download source code
+      </a>
+      <a v-if="getLesson.downloadUrl"
+      :href="getLesson.downloadUrl"
+      >
+      Download lesson
+      </a>
+    </div>
+  </div>
+  <div class="text-section">
     <p>{{ getLesson.text }}</p>
   </div>
 </template>
 
 <style scoped>
+
+.lesson-number p {
+  text-transform: uppercase;
+  color: rgb(157, 156, 156)
+}
 
 </style>
